693f85fc79bf942e52ae9ea86ebb68064e4aad4e
hama1185 <hamataku0120@gmail.com>
2021-07-03 09:29:40 +0900

    change path

--- quickstarts/conversion/ConversionQuickstart/Configuration.cs
+++ quickstarts/conversion/ConversionQuickstart/Configuration.cs
@@ -14,16 +14,16 @@ namespace ConversionQuickstart
     public class Configuration
     {
         // Azure Object Anchors account identifier
-        public string AccountId = "set-me";
+        public string AccountId = "75346a1c-6b6e-404e-8446-7db79f65f2b8";
 
         // Azure Object Anchors account primary key
-        public string AccountKey = "set-me";
+        public string AccountKey = "z7Ld2AHQVFLe23ZbV+IUl45AsQP5ad7laQhouIBceT8=";
 
         // Azure Object Anchors account domain
-        public string AccountDomain = "set-me";
+        public string AccountDomain = "eastus2.mixedreality.azure.com";
 
         // Path to 3D asset file on your local machine
-        public string InputAssetPath = "set-me";
+        public string InputAssetPath = "C:/Users/hamat/prog/azure-object-anchors/quickstarts/apps/unity/mrtk/Model/hondana.obj";
 
         // Timeout to wait for job completion
         public TimeSpan WaitForJobCompletionTimeout = TimeSpan.FromMinutes(40);
